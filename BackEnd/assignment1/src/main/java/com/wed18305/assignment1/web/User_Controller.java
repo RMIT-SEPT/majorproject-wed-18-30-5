c4c05f4d4a401b528abe21a434dd501108afb39d
rmit-s3561388-Neil-Kennedy <s3561388@student.rmit.edu.au>
2020-08-10 12:33:40 +1000

    Commenting example data for user_controller

--- BackEnd/assignment1/src/main/java/com/wed18305/assignment1/web/User_Controller.java
+++ BackEnd/assignment1/src/main/java/com/wed18305/assignment1/web/User_Controller.java
@@ -19,8 +19,16 @@ public class User_Controller{
     @Autowired
     private User_Service userService;
 
+    // Create new user
+    // POST ENDPOINT: http://localhost:8080/api/user 
+    // JSON {"name":"neil",
+    //       "username":"neil",
+    //       "password":"1234",
+    //       "contactNumber":"0425000000"} example format
+    // UserType NOT CURRENTLY IMPLEMENTED 
     @PostMapping()
     public ResponseEntity<User> createNewUser(@RequestBody User user){
+        //TODO check JSON formatting and that all parameters are supplied.
         User user1 = userService.saveOrUpdateUser(user);
         if(user1 == null){
             return new ResponseEntity<User>(user1, HttpStatus.CREATED);
@@ -29,8 +37,13 @@ public class User_Controller{
         }
     }
 
+    // Get user via username and password
+    // GET ENDPOINT: http://localhost:8080/api/user   
+    // JSON{"name":"neil",
+    //      "password":"1234"} example format
     @GetMapping()
     public ResponseEntity<Boolean> findByUser(@RequestBody User user){
+        //TODO check JSON formatting and that name, password parameters are supplied.
         User user1 = userService.findByNameAndPassword(user.getName(), user.getPassword());
         if(user1 == null){
             return new ResponseEntity<Boolean>(false, HttpStatus.NOT_FOUND);
