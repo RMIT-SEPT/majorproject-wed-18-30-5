db493f087a718c2b13e28cee492e1b531f76f336
[Moditha Sulakshana s3756843] <s3756843@student.rmit.edu.au>
2020-09-03 04:42:47 +1000

    booking page design

--- FrontEnd/src/components/Home.js
+++ FrontEnd/src/components/Home.js
@@ -1,81 +1,34 @@
 import React, { Component } from "react";
 import ApiService from "../api/ApiService";
+import NavBar from "./NavBar";
+import Booking from "./Booking";
+import background from "../images/background.jpg";
 
 class Home extends Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      users: [],
-      message: null
-    }
 
-  }
 
-  componentDidMount() {
-    this.reloadUserList();
-  }
-
-  reloadUserList = () => {
-    ApiService.fetchUsers()
-      .then((res) => {
-        this.setState({ users: res.data.body })
-      });
-  }
-
-  deleteUser = (userId) => {
-    ApiService.deleteUser(userId)
-      .then(res => {
-        this.setState({ message: 'User deleted successfully.' });
-        this.setState({ users: this.state.users.filter(user => user.id !== userId) });
-      })
-
-  }
-
-  editUser = (id) => {
-    window.localStorage.setItem("userId", id);
-    this.props.history.push('/edit-user');
-  }
-
-  addUser = () => {
-    window.localStorage.removeItem("userId");
-    this.props.history.push('/add-user');
-  }
   render() {
     return (
-      <div>
-        <h2 className="text-center">User Details</h2>
-        <button className="btn btn-danger" onClick={() => this.addUser()}> Add User</button>
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th className="hidden">Id</th>
-              <th>Name</th>
-              <th>Username</th>
-              <th>Password</th>
-              <th>ContactNumber</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              this.state.users.map(
-                user =>
-                  <tr key={user.username}>
-                    <td>{user.name}</td>
+      <>
 
-                    <td>{user.password}</td>
-                    <td>{user.contactNumber}</td>
-                    <td>
-                      <button className="btn btn-success" onClick={() => this.deleteUser(user.id)}> Delete</button>
-                      <button className="btn btn-success" onClick={() => this.editUser(user.id)}> Edit</button>
-                    </td>
-                  </tr>
-              )
-            }
-          </tbody>
-        </table>
+        <header>
+          <NavBar />
+        </header>
+        <section>
+          <div className="order-1 order-lg-2">
+            <img src={background} className="img-fluid home-bq" />
+            <div className="booking-wrapper">
+              <Booking />
+            </div>
+          </div>
 
-      </div>
+
+
+        </section>
+
+      </>
+
     );
   }
 
