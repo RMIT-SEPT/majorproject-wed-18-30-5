1f2fe22e6900c8c533f9858cc781e3b6075b10ca
michaelAnning <s3720132@student.rmit.edu.au>
2020-09-08 02:56:10 +1000

    Booking_Request validation implemented. Null fix.

--- BackEnd/assignment1/src/main/java/com/wed18305/assignment1/Requests/Booking_Request.java
+++ BackEnd/assignment1/src/main/java/com/wed18305/assignment1/Requests/Booking_Request.java
@@ -1,20 +1,21 @@
 package com.wed18305.assignment1.Requests;
 
 import java.time.OffsetDateTime;
-// import javax.validation.constraints.NotNull;
+
+import javax.validation.constraints.NotNull;
 
 import com.wed18305.assignment1.config.DateTimeStatic;
 
 public class Booking_Request {
 
     /// Variables
-    // @NotBlank(message = "A StartDate is required.")
+    @NotNull(message = "A StartDate is required.")
     protected OffsetDateTime startDateTime;
-    // @NotBlank(message = "A EndDate is required.")
+    @NotNull(message = "A EndDate is required.")
     protected OffsetDateTime endDateTime;
-    // @NotBlank(message = "Please assign a customer to this booking.")
+    @NotNull(message = "Please assign a customer to this booking.")
     protected Long[] customer_ids;
-    // @NotBlank(message = "Please assign a worker to this booking.")
+    @NotNull(message = "Please assign a worker to this booking.")
     protected Long[] employees_ids;
     
     //Constructors
