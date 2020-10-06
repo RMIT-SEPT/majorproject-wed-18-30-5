70b80cba43a8eb2ab89e0b4aa2f48edab50e796d
michaelAnning <s3720132@student.rmit.edu.au>
2020-10-07 01:09:21 +1100

    Can't cancel bookings starting within 48 hours.

--- BackEnd/assignment1/src/main/java/com/wed18305/assignment1/config/DateTimeStatic.java
+++ BackEnd/assignment1/src/main/java/com/wed18305/assignment1/config/DateTimeStatic.java
@@ -7,8 +7,4 @@ public class DateTimeStatic {
         // "Year, Month, day, Hour, Minute." e.g '2020-09-07T17:00+10:00'
         return DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mmXXXXX");
     }
-
-    public static String getUTCOffset() {
-        return "+10:00";
-    }
 }
